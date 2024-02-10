e05d9835816eb8f90758caf88e1fdfd41f030a4e
Haydar Çoban <hydr.cbn@gmail.com>
2024-02-10 20:56:29 +0300

    React Card Project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "bulma/css/bulma.css";
+import "./App.css";
+import Course from "./Course";
+import angular from "./images/dev1.jpg";
+import Bootstrap from "./images/dev2.jpg";
+import ccsharp from "./images/dev3.jpg";
+import kompledev from "./images/dev4.jpg";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section className="hero is-link">
+        <div className="hero-body">
+          <p className="title">Kurslarım</p>
+        </div>
+      </section>
+      <div className="container">
+        <section className="section">
+          <div className="columns">
+            <div className="column">
+              {" "}
+              <Course
+                image={angular}
+                title="angular"
+                description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo odio accusamus minima soluta, fugit fugiat et non sed veritatis architecto modi ea vero, explicabo beatae suscipit, dignissimos sequi dolores in!"
+              />
+            </div>
+            <div className="column">
+              <Course
+                image={Bootstrap}
+                title="java"
+                description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo odio accusamus minima soluta, fugit fugiat et non sed veritatis architecto modi ea vero, explicabo beatae suscipit, dignissimos sequi dolores in!"
+              />
+            </div>
+            <div className="column">
+              <Course
+                image={ccsharp}
+                title="Komplo Web"
+                description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo odio accusamus minima soluta, fugit fugiat et non sed veritatis architecto modi ea vero, explicabo beatae suscipit, dignissimos sequi dolores in!"
+              />
+            </div>
+            <div className="column">
+              <Course
+                image={kompledev}
+                title="Front end"
+                description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo odio accusamus minima soluta, fugit fugiat et non sed veritatis architecto modi ea vero, explicabo beatae suscipit, dignissimos sequi dolores in!"
+              />
+            </div>
+          </div>
+        </section>
+      </div>
     </div>
   );
 }
